7888251773811a7987c2d81ed6955800619cdb74
Lisa Han <ellesis@gmail.com>
2020-08-16 07:12:21 -0700

    add backgroundSection

---
+++ src/components/globals/backgroundSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BackgroundSection extends Component {
+  render() {
+    return (
+      <div>
+        Hello from background
+      </div>
+    );
+  }
+}
+
+export default BackgroundSection;
